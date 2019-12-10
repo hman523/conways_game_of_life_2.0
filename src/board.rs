06d00fd434a3d3f702b37ce071d3b7bc45c04110
hman523 <shbarbella@gmail.com>
2019-12-10 18:37:37 -0500

    formatted code

--- src/board.rs
+++ src/board.rs
@@ -1,114 +1,113 @@
-extern crate rand;
 extern crate colored;
-use rand::Rng;
+extern crate rand;
 use colored::*;
+use rand::Rng;
 
 const SIZEX: usize = 80;
 const SIZEY: usize = 22;
 
 pub struct Board {
-	board: [[bool; SIZEX]; SIZEY]
+    board: [[bool; SIZEX]; SIZEY],
 }
 
 impl Board {
-	pub fn new() -> Board {
-		return Board { board: [[false; SIZEX]; SIZEY]};
-	}
-	
-	pub fn count(&self) -> usize{
-		let mut count = 0;
-		for(_i, row) in self.board.iter().enumerate() {
-			count += row.iter().filter(|&x| *x == true).count();
-		}
-		return count;
-	}
-	
-	pub fn next(&self) -> Board{
-		let mut newmap = [[false; SIZEX]; SIZEY];
-		for (i, row) in self.board.iter().enumerate() {
-			for (j, _col) in row.iter().enumerate() {
-				newmap[i][j] = self.will_live(i, j);
-			}
-		}
+    pub fn new() -> Board {
+        return Board {
+            board: [[false; SIZEX]; SIZEY],
+        };
+    }
 
-		return Board { board: newmap };
-	}
+    pub fn count(&self) -> usize {
+        let mut count = 0;
+        for (_i, row) in self.board.iter().enumerate() {
+            count += row.iter().filter(|&x| *x == true).count();
+        }
+        return count;
+    }
 
-	fn count_neighbors(&self, x: usize, y: usize) -> i32 {
-		let mut count = 0;
-		if x != 0 && self.board[x-1][y]{
-			count += 1;
-		}
-		if x != 0 && y != 0 && self.board[x-1][y-1]{
-			count += 1;
-		}
-		if y != 0 && self.board[x][y-1]{
-			count += 1;
-		}
-		if y != 0 && x != SIZEY-1 && self.board[x+1][y-1]{
-			count += 1;
-		}
-		if y != SIZEX-1 && x != SIZEY-1 && self.board[x+1][y+1]{
-			count += 1;
-		}
-		if x != SIZEY-1 && self.board[x+1][y]{
-			count += 1;
-		}
-		if x != 0 && y != SIZEX-1 && self.board[x-1][y+1]{
-			count += 1;
-		}
-		if y != SIZEX-1 && self.board[x][y+1]{
-			count += 1;
-		}
+    pub fn next(&self) -> Board {
+        let mut newmap = [[false; SIZEX]; SIZEY];
+        for (i, row) in self.board.iter().enumerate() {
+            for (j, _col) in row.iter().enumerate() {
+                newmap[i][j] = self.will_live(i, j);
+            }
+        }
 
-		return count;
-	}	
+        return Board { board: newmap };
+    }
 
-	fn will_live(&self, x: usize, y: usize) -> bool{
-		let neighbors = self.count_neighbors(x, y);
-		
-		let is_alive = self.board[x][y];
-		if !is_alive {
-			if neighbors == 3{
-				return true;
-			}
-		}
-		else{
-			if neighbors == 2 || neighbors == 3 {
-				return true;
-			}
-		}
-		return false;
-	}
+    fn count_neighbors(&self, x: usize, y: usize) -> i32 {
+        let mut count = 0;
+        if x != 0 && self.board[x - 1][y] {
+            count += 1;
+        }
+        if x != 0 && y != 0 && self.board[x - 1][y - 1] {
+            count += 1;
+        }
+        if y != 0 && self.board[x][y - 1] {
+            count += 1;
+        }
+        if y != 0 && x != SIZEY - 1 && self.board[x + 1][y - 1] {
+            count += 1;
+        }
+        if y != SIZEX - 1 && x != SIZEY - 1 && self.board[x + 1][y + 1] {
+            count += 1;
+        }
+        if x != SIZEY - 1 && self.board[x + 1][y] {
+            count += 1;
+        }
+        if x != 0 && y != SIZEX - 1 && self.board[x - 1][y + 1] {
+            count += 1;
+        }
+        if y != SIZEX - 1 && self.board[x][y + 1] {
+            count += 1;
+        }
 
-	fn random(&self) -> bool {
-		let mut rng = rand::thread_rng();
-		return rng.gen_range(0, 2) == 1;
-	}
+        return count;
+    }
 
-	pub fn fill_random(&self) -> Board {
-		let mut newmap = [[false; SIZEX]; SIZEY];
-		for (i, row) in self.board.iter().enumerate() {
-			for (j, _col) in row.iter().enumerate() {
-				newmap[i][j] = self.random();
-			}
-		}
+    fn will_live(&self, x: usize, y: usize) -> bool {
+        let neighbors = self.count_neighbors(x, y);
 
-		return Board { board: newmap };
+        let is_alive = self.board[x][y];
+        if !is_alive {
+            if neighbors == 3 {
+                return true;
+            }
+        } else {
+            if neighbors == 2 || neighbors == 3 {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	}
+    fn random(&self) -> bool {
+        let mut rng = rand::thread_rng();
+        return rng.gen_range(0, 2) == 1;
+    }
 
-	pub fn print(&self){
-		for (_i, row) in self.board.iter().enumerate() {
-			for (_j, col) in row.iter().enumerate() {
-				if *col{
-					print!("{}", "#".green());
-				}
-				else{
-					print!(" ");
-				}
-			}
-			println!();
-		}
-	}
+    pub fn fill_random(&self) -> Board {
+        let mut newmap = [[false; SIZEX]; SIZEY];
+        for (i, row) in self.board.iter().enumerate() {
+            for (j, _col) in row.iter().enumerate() {
+                newmap[i][j] = self.random();
+            }
+        }
+
+        return Board { board: newmap };
+    }
+
+    pub fn print(&self) {
+        for (_i, row) in self.board.iter().enumerate() {
+            for (_j, col) in row.iter().enumerate() {
+                if *col {
+                    print!("{}", "#".green());
+                } else {
+                    print!(" ");
+                }
+            }
+            println!();
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,41 @@
 extern crate colored;
 mod board;
 use board::Board;
-use std::{thread, time};
 use colored::*;
+use std::{thread, time};
 
 fn main() {
     println!("Starting the simulation");
-	let mut b = Board::new();
-	b = b.fill_random();
-	let mut generation = 0;
-	let mut maxpop = 0;
-	loop{	
-		clear();
-		let count = b.count();
-		maxpop = std::cmp::max(maxpop, count);
-		print!("{}{}\t\t", "Generation: ".red(), 
-		generation.to_string().blue());
-		print!("{}{}\t\t", "Population: ".red(), 
-		count.to_string().blue());
-		print!("{}{}\n", "Max Population: ".red(), 
-		maxpop.to_string().blue());
-		b.print();
-		b = b.next();
-		generation += 1;
-		pause();
-	}
+    let mut b = Board::new();
+    b = b.fill_random();
+    let mut generation = 0;
+    let mut maxpop = 0;
+    loop {
+        clear();
+        let count = b.count();
+        maxpop = std::cmp::max(maxpop, count);
+        print!(
+            "{}{}\t\t",
+            "Generation: ".red(),
+            generation.to_string().blue()
+        );
+        print!("{}{}\t\t", "Population: ".red(), count.to_string().blue());
+        print!(
+            "{}{}\n",
+            "Max Population: ".red(),
+            maxpop.to_string().blue()
+        );
+        b.print();
+        b = b.next();
+        generation += 1;
+        pause();
+    }
 }
 
-fn pause(){
-	thread::sleep(time::Duration::from_millis(200));
+fn pause() {
+    thread::sleep(time::Duration::from_millis(200));
 }
 
 fn clear() {
-	print!("{}[2J", 27 as char);
+    print!("{}[2J", 27 as char);
 }
